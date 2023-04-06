59833925df72d66bfaa427b81fba57f434be22be
quentin-metro <quentin.metro@gmail.com>
2023-04-06 16:10:32 +0200

    carousel done !

--- juststreamit.js
+++ juststreamit.js
@@ -3,26 +3,29 @@ url2 = "http://127.0.0.1:5000/bestalltime"
 url4 = "http://127.0.0.1:5000/worst"
 url3 = "http://127.0.0.1:5000/genre/romance"
 
-const createImgInDiv = function(id_block,film_url,film_id){
+const createImgInDiv = function(div_id,film_url,film_id){
     let img = document.createElement("img")
     img.id = "#film" +film_id
-    img.className = "test"
+    img.className = "filmimage"
     img.src = film_url || "juststreamit.png"
-    let block = document.getElementById(id_block);
-    block.appendChild(img);
+    img.setAttribute("onclick","modal(" + film_id + ")");
+    let div = document.getElementById(div_id);
+    div.appendChild(img);
 }
 
-const createAFilm = function(id_carousel,film_url,film_id){
+const createAFilm = function(id_carousel,film_data){
+    let block = document.getElementById(id_carousel)
     let div = document.createElement("div")
+    div.id = '#f' + film_data["id"]
     div.className = "itemImage"
-    let block2 = document.getElementById(id_carousel)
-    block2.appendChild(div)
-    let a = document.createElement("a")
-    a.className = "js-modal"
-    a.id='#f'+id_carousel+film_id
-    a.href = '#'
-    div.appendChild(a)
-    createImgInDiv(a.id,film_url,film_id)
+    // Only the first image in a slideshow is visible
+    if (block.getElementsByClassName("itemImage").length == 0){
+        div.style.display = "block"
+    } else {
+        div.style.display = "none"
+    }
+    block.appendChild(div)
+    createImgInDiv(div.id, film_data["image_url"], film_data["id"])
 }
 
 const best8film = async function(){
@@ -30,14 +33,14 @@ const best8film = async function(){
     let response = await fetch(url)
     let film_data = await response.json()
     // The best in picture
+    let block1 = document.getElementById("best1")
     let h3 = document.createElement("h3")
     h3.innerHTML = film_data["0"]["title"]
-    let block1 = document.getElementById("best1")
     block1.appendChild(h3)
-    createImgInDiv("best1",film_data["0"]["image_url"],film_data["0"]["id"])
+    createAFilm("best1",film_data["0"])
     // 7 other best film
     for (let i=1; i <= 7; i++){
-        createAFilm("best",film_data[i]["image_url"],film_data[i]["id"])
+        createAFilm("best",film_data[i])
     }
 }
 
@@ -46,7 +49,7 @@ const best8alltime = async function(){
     let response = await fetch(url)
     let film_data = await response.json()
     for (let i=0; i <= 7; i++){
-        createAFilm("bestalltime",film_data[i]["image_url"],film_data[i]["id"])
+        createAFilm("bestalltime",film_data[i])
     }
 }
 
@@ -55,7 +58,7 @@ const worst8film = async function(){
     let response = await fetch(url)
     let film_data = await response.json()
     for (let i=0; i <= 7; i++){
-        createAFilm("worst",film_data[i]["image_url"],film_data[i]["id"])
+        createAFilm("worst",film_data[i])
     }
 }
 
@@ -64,11 +67,14 @@ const best8romance = async function(){
     let response = await fetch(url)
     let film_data = await response.json()
     for (let i=0; i <= 7; i++){
-        createAFilm("romance",film_data[i]["image_url"],film_data[i]["id"])
+        createAFilm("romance",film_data[i])
     }
 }
 
-best8film()
-best8alltime()
-worst8film()
-best8romance()
+
+best8film();
+best8alltime();
+worst8film();
+best8romance();
+const event = new Event("ApiCall");
+document.dispatchEvent(event);
